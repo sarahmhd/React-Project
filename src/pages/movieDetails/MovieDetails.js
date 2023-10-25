a48d0c820d3cceba5061fecca0a969ba70d136f6
sarahmhd <sarahmhd213199@gmail.com>
2023-10-25 23:04:17 +0200

    details

--- src/pages/movieDetails/MovieDetails.js
+++ src/pages/movieDetails/MovieDetails.js
@@ -29,38 +29,47 @@ const MovieDetails = () => {
             alt={movie.title}
           />
         </div>
-        <div className="top-details">
-          <div className="details-content d-flex">
-            <div className="img">
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                alt=""
-              />
-            </div>
+        <div className="top-details row">
+          <div className="img col-lg-4">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt=""
+            />
+          </div>
+          <div className="top-details-info col-lg-8 d-flex flex-column">
             <div className="publish-date">
-              {movie.release_date.substring(0, 4)}
+              {movie.release_date?.substring(0, 4)}
             </div>
             <h2 className="title">
               {movie.original_title || movie.original_name}
             </h2>
             <p className="description">{movie.overview}</p>
-            <div className="more-info d-flex">
-              <div className="genres d-flex">
+            <div className="more-info d-flex gap-2">
+              <div className="genres d-flex gap-2">
                 {movie?.genres?.map((genre) => {
                   return <span>{genre.name}</span>;
                 })}
               </div>
-              <div className="release-data">{movie.release_date}</div>
+              <div className="release-data">
+                {new Date(movie.release_date).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </div>
             </div>
-            <div className="status-info">
+            <div className="status-info d-flex justify-content-between align-items-center">
               <span className="rate">
-                {movie.vote_average?.toFixed(1)} IMDb
+                <span className="rate-val">
+                  {movie.vote_average?.toFixed(1)}{" "}
+                </span>
+                IMDb
               </span>
               <div className="status d-flex flex-column">
                 <span className="status-text">status</span>
                 <span className="status-val">{movie.status}</span>
               </div>
-              <div className="btns d-flex">
+              <div className="btns d-flex align-items-center gap-2">
                 <button className="btn add">
                   <AddIcon />
                 </button>
